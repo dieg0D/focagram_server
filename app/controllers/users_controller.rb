c020dd2deff58ac13a504c8a16297a73957138ec
dieg0d <diegodi05@hotmail.com>
2020-02-17 07:28:54 -0300

    arrumando create de user pra ele ja logar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, except: [:password_digest], status: :created
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                     user_id: @user.id }, status: :created
     else
       render json: { errors: @user.errors.full_messages },
              status: :unprocessable_entity
